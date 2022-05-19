6f8b957a0f76f9aec217ae3d0181f94431f14bc8
Zulkarnaen <Zulkarnaim70@gmail.com>
2022-05-19 11:53:08 +0700

    Belejar variable shadowing dan this keyword

---
+++ bin/variable_shadowing.dart
@@ -0,0 +1,23 @@
+//methed yang di panggil ketika sebuah object di buat, ketia membuat controkter otomatis memaniggil
+//singkat biar class bisa mendapatkan parameter
+class Person{
+ 
+ String name ='guest';
+ String? address;
+ final String country="Indonesia";
+
+Person(String name, address){
+name = name; 
+address = address;
+}
+
+}
+void main(){
+
+var person = Person('Zulkarnaen', 'jakarta');
+
+print(person.name);
+print(person.address);
+
+
+}
