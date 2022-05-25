023be1990059d32222dea5f76d9774aebce8b717
magerngulik <magerngulik@gmail.com>
2022-05-25 18:04:30 +0700

    multiple try and catch

--- bin/exception.dart
+++ bin/exception.dart
@@ -4,9 +4,6 @@ class ValidationExeption implements Exception{
   ValidationExeption(this.message);
 }
 
-
-
-
 class Validation{
   static void validate(String username,String password){
     //kelas exception yang ada di dart
@@ -16,16 +13,20 @@ class Validation{
       //cek data yang di input benar atau tidak
     }else if (password =="") {
       throw ValidationExeption("password is blank");
+    } else if (username != 'eko' || password !='eko' ) {
+      throw Exception('Login Failed');
     }
   }
 }
 
 void main(List<String> args) {
+  //untuk penaganan error
   try{
-  Validation.validate("ada isi nya","lalal");
-  } on ValidationExeption{
-    print('Validation Error');
+  Validation.validate("ada isi nya","111");
+  } on ValidationExeption catch (exception){
+    print('Validation Error: ${exception.message}');
+  } on Exception catch (exception){
+    print('Error: ${exception.toString()}');    
   }
-
   print("Selesai");
 }
