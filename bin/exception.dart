db664015f77f8fd1eb959467ef2bfb29b6cd803d
magerngulik <magerngulik@gmail.com>
2022-05-25 17:54:52 +0700

    try merupakan logic jika terjadi error pada validation exeption

--- bin/exception.dart
+++ bin/exception.dart
@@ -1,16 +1,31 @@
+//harus di flaging/ditandai dengan kelas exeption
+class ValidationExeption implements Exception{
+  String message;
+  ValidationExeption(this.message);
+}
+
+
+
 
 class Validation{
   static void validate(String username,String password){
     //kelas exception yang ada di dart
+    //ganti dari exeption biasa ke validation exeption
     if (username =="") {
-      throw Exception("username is blank");
+      throw ValidationExeption("username is blank");
       //cek data yang di input benar atau tidak
     }else if (password =="") {
-      throw Exception("password is blank");
+      throw ValidationExeption("password is blank");
     }
   }
 }
 
 void main(List<String> args) {
-  Validation.validate("ada isi nya","");
+  try{
+  Validation.validate("ada isi nya","lalal");
+  } on ValidationExeption{
+    print('Validation Error');
+  }
+
+  print("Selesai");
 }
